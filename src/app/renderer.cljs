8d955c58b370fdfb0a1a991838e3a02074b5fc4b
Bost <thebost@gmail.com>
2017-09-03 23:18:44 +0200

    Cleanup comments

--- src/app/renderer.cljs
+++ src/app/renderer.cljs
@@ -12,83 +12,27 @@
 ;; A detailed walk-through of this source code is provied in the docs:
 ;; https://github.com/Day8/re-frame/blob/master/docs/CodeWalkthrough.md
 
-;; -- Domino 1 - Event Dispatch -----------------------------------------------
-
 (defn dispatch-timer-event
   []
-  (let [now (js/Date.)]
-    (rf/dispatch [:timer now])))  ;; <-- dispatch used
+  (let [now (js/Date.)] (rf/dispatch [:timer now])))
 
 ;; Call the dispatching function every second.
 ;; `defonce` is like `def` but it ensures only one instance is ever
 ;; created in the face of figwheel hot-reloading of this file.
 (defonce do-timer (js/setInterval dispatch-timer-event 1000))
 
+(rf/reg-event-db :initialize (fn [_ _] {:time (js/Date.) :time-color "#f88"}))
+(rf/reg-event-db :data-change (fn [db [_ new-data]] (assoc db :data new-data)))
+(rf/reg-event-db :cm-change (fn [db [_ new-cm]] (assoc db :cm new-cm)))
+(rf/reg-event-db :fname-change (fn [db [_ new-fname]] (assoc db :fname new-fname)))
+(rf/reg-event-db :time-color-change (fn [db [_ new-color-value]] (assoc db :time-color new-color-value)))
+(rf/reg-event-db :timer (fn [db [_ new-time]] (assoc db :time new-time)))
 
-;; -- Domino 2 - Event Handlers -----------------------------------------------
-
-(rf/reg-event-db             ;; sets up initial application state
- :initialize                 ;; usage:  (dispatch [:initialize])
- (fn [_ _]                    ;; the two parameters are not important here, so use _
-   {
-    ;; :fname "ufo"
-    :time (js/Date.)         ;; What it returns becomes the new application state
-    :time-color "#f88"}))    ;; so the application state will initially be a map with two keys
-
-
-(rf/reg-event-db
- :data-change
- (fn [db [_ new-data]] (assoc db :data new-data)))
-
-(rf/reg-event-db
- :cm-change
- (fn [db [_ new-cm]] (assoc db :cm new-cm)))
-
-(rf/reg-event-db
- :fname-change
- (fn [db [_ new-fname]] (assoc db :fname new-fname)))
-
-(rf/reg-event-db              ;; usage:  (dispatch [:time-color-change 34562])
- :time-color-change           ;; dispatched when the user enters a new colour into the UI text field
- (fn [db [_ new-color-value]]  ;; -db event handlers given 2 parameters:  current application state and event (a vector)
-   (assoc db :time-color new-color-value)))   ;; compute and return the new application state
-
-
-(rf/reg-event-db                ;; usage:  (dispatch [:timer a-js-Date])
- :timer                         ;; every second an event of this kind will be dispatched
- (fn [db [_ new-time]]           ;; note how the 2nd parameter is destructured to obtain the data value
-   (assoc db :time new-time)))  ;; compute and return the new application state
-
-
-;; -- Domino 4 - Query  -------------------------------------------------------
-
-(rf/reg-sub
- :data
- (fn [db _]      ;; db is current app state. 2nd unused param is query vector
-   (:data db)))
-
-(rf/reg-sub
- :cm
- (fn [db _]      ;; db is current app state. 2nd unused param is query vector
-   (:cm db)))
-
-(rf/reg-sub
- :fname
- (fn [db _]      ;; db is current app state. 2nd unused param is query vector
-   (:fname db)))
-
-(rf/reg-sub
- :time
- (fn [db _]      ;; db is current app state. 2nd unused param is query vector
-   (:time db))) ;; return a query computation over the application state
-
-(rf/reg-sub
- :time-color
- (fn [db _]
-   (:time-color db)))
-
-
-;; -- Domino 5 - View Functions ----------------------------------------------
+(rf/reg-sub :data (fn [db _] (:data db)))
+(rf/reg-sub :cm (fn [db _] (:cm db)))
+(rf/reg-sub :fname (fn [db _] (:fname db)))
+(rf/reg-sub :time (fn [db _] (:time db)))
+(rf/reg-sub :time-color (fn [db _] (:time-color db)))
 
 (defn clock
   []
@@ -105,7 +49,7 @@
    "Time color: "
    [:input {:type "text"
             :value @(rf/subscribe [:time-color])
-            :on-change #(rf/dispatch [:time-color-change (-> % .-target .-value)])}]])  ;; <---
+            :on-change #(rf/dispatch [:time-color-change (-> % .-target .-value)])}]])
 
 (defn save [fs fname data]
   (.writeFile fs fname data
@@ -188,12 +132,10 @@
    [:div "after"]
    ])
 
-
-;; -- Entry Point -------------------------------------------------------------
-
 (defn ^:export run
   []
-  (rf/dispatch-sync [:initialize])  ;; puts a value into application state
-  (reagent/render [ui]              ;; mount the application's ui into '<div id="app" />'
-                  (js/document.getElementById "app")))
+  ;; puts a value into application state
+  (rf/dispatch-sync [:initialize])
+  ;; mount the application's ui into '<div id="app" />'
+  (reagent/render [ui] (js/document.getElementById "app")))
 
