06ae843b2905e8cace2c024fbe5f63effd34cfaa
Bost <thebost@gmail.com>
2017-09-25 16:36:32 +0200

    defmacro key-comb doesn't work

--- src/app/keymap.cljs
+++ src/app/keymap.cljs
@@ -24,107 +24,48 @@
        (rf/dispatch [:prev-file-change active])
        (rf/dispatch [:active-file-change prev])))))
 
-(defn key-comb [k f & args]
-  {k
-   (fn [editor]
-     (.log js/console (name k))
-     (apply f args))})
+#_(defn key-comb [k f & args]
+  {k (fn []
+       ;; (.log js/console "key-comb" "editor" editor)
+       #_(.log js/console (name k))
+       (apply f args))})
+
+(defmacro key-comb [k fun]
+  `(let [xk# ~k
+         xfun# ~fun]
+     (.log js/console (name (quote ~k)))
+     {(quote ~k) (quote ~fun)}))
 
 (defn keymap
   "CodeMirror only keymap. Global shortcuts must be configured elsewhere"
   [file open-files]
+  (key-comb :Cmd-D (fn [editor] (fs/exec ["ls" "-la"])))
   (->>
-   (conj
+   (dbg {:Cmd-D (fn [editor] (fs/exec ["ls" "-la"]))})
+   #_(conj
     {}
     (key-comb :Cmd-Ctrl-Up (fn [editor] (rf/dispatch [:tabs-pos-change css/tabs-on-top])))
-    {
-     ;; :Ctrl-W (fn [editor] (.log js/console "Ctrl-W"))
-
-     ;; single key: <S>
-     ;; :Mod (fn [editor] (.log js/console "Mod"))
-
-     ;; :F11
-     ;; (fn [editor]
-     ;;   (.log js/console "F11")
-     ;;   (.log js/console "Full screen: Can be stolen"))
-     #_:Cmd-Ctrl-Up
-     #_(fn [editor]
-         (.log js/console "Cmd-Ctrl-Up / <s-C-up>")
-         (rf/dispatch [:tabs-pos-change css/tabs-on-top]))
-     :Cmd-Ctrl-Down
-     (fn [editor]
-       (.log js/console "Cmd-Ctrl-Down / <s-C-down>")
-       (rf/dispatch [:tabs-pos-change css/no-tabs])
-       )
-     :Cmd-Ctrl-Left
-     (fn [editor]
-       (.log js/console "Cmd-Ctrl-Left / <s-C-left>")
-       (rf/dispatch [:tabs-pos-change css/left-to-right]))
-     :Cmd-Ctrl-Right
-     (fn [editor]
-       (.log js/console "Cmd-Ctrl-Right / <s-C-right>")
-       (rf/dispatch [:tabs-pos-change css/right-to-left]))
-     :Cmd-F
-     (fn [editor]
-       (.log js/console "Cmd-F / <S-f>")
-       ;; assigning file to file causes file-reload
-       (rf/dispatch [:active-file-change file])
-       (fs/read file editor open-files))
-     ;; :Ctrl-R
-     ;; (fn [editor]
-     ;;   (.log js/console "Ctrl-R / <C-r>")
-     ;;   (.log js/console "Can be stolen"))
-     ;; :Shift-Ctrl-I
-     ;; (fn [editor]
-     ;;   (.log js/console "Shif-Ctrl-I / <C-S-i>")
-     ;;   (.log js/console "Can be stolen"))
-     :Cmd-Tab
-     (fn [editor]
-       (.log js/console "Cmd-Tab / <s-tab>")
-       (alternate-active editor open-files))
-     :Cmd-S
-     (fn [editor]
-       (.log js/console "Cmd-S / <S-s>")
-       (fs/save file (.getValue (.-doc editor))))
-     :Shift-Ctrl-S
-     (fn [editor]
-       (.log js/console "Shift-Ctrl-S")
-       (fs/save-ide-settings))
-     :Cmd-Q
-     (fn [editor]
-       (.log js/console "Cmd-Q / <S-q>")
-       (next-active editor open-files))
-     :Cmd-Left
-     (fn [editor]
-       (.log js/console "Cmd-Left")
-       (p/backward-sexp editor))
-     :Cmd-Right
-     (fn [editor]
-       (.log js/console "Cmd-Right")
-       (p/forward-sexp editor))
-     :Cmd-Up
-     (fn [editor]
-       (.log js/console "Cmd-Up")
-       (p/forward-sexp editor))
-     :Cmd-Ctrl-Alt-P
-     (fn [editor]
-       (.log js/console "Cmd-Ctrl-Alt-P")
-       #_(p/forward-sexp editor))
-     :Cmd-Ctrl-Alt-K
-     (fn [editor]
-       (.log js/console "Cmd-Ctrl-Alt-K")
-       (fs/exec ["pkill" "--full" "boot"]))
-     :Cmd-Ctrl-Alt-L
-     (fn [editor]
-       (.log js/console "Cmd-Ctrl-Alt-L")
-       (fs/exec ["pgrep" "--full" "boot"]))
-     :Shift-Cmd-D
-     (fn [editor]
-       (.log js/console "Shift-Cmd-D")
-       (fs/exec ["ls" "-la"]))
-     :Cmd-Ctrl-Alt-B
-     (fn [editor]
-       (.log js/console "Cmd-Ctrl-Alt-B")
-       (fs/exec ["boot" "watch" "dev-build"]))
-     })
+    #_(key-comb :Ctrl-W (fn [editor]))
+    #_(key-comb :Mod (fn [editor])) ;; single key: <S>
+    #_(key-comb :F11 (fn [editor] (.log js/console "Full screen: Can be stolen")))
+    (key-comb :Cmd-Ctrl-Down (fn [editor] (rf/dispatch [:tabs-pos-change css/no-tabs])))
+    (key-comb :Cmd-Ctrl-Left (fn [editor] (rf/dispatch [:tabs-pos-change css/left-to-right])))
+    (key-comb :Cmd-Ctrl-Right (fn [editor] (rf/dispatch [:tabs-pos-change css/right-to-left])))
+    (key-comb :Cmd-F (fn [editor] ;; assigning file to file causes file-reload
+                       (rf/dispatch [:active-file-change file])
+                       (fs/read file editor open-files)))
+     #_(key-comb :Ctrl-R (fn [editor] (.log js/console "Can be stolen")))
+     #_(key-comb :Shift-Ctrl-I (fn [editor] (.log js/console "Can be stolen")))
+     (key-comb :Cmd-Tab (fn [editor] (alternate-active editor open-files)))
+     (key-comb :Cmd-S (fn [editor] (fs/save file (.getValue (.-doc editor)))))
+     (key-comb :Shift-Ctrl-S (fn [editor] (fs/save-ide-settings)))
+     (key-comb :Cmd-Q (fn [editor] (next-active editor open-files)))
+     (key-comb :Cmd-Left (fn [editor] (p/backward-sexp editor)))
+     (key-comb :Cmd-Right (fn [editor] (p/forward-sexp editor)))
+     (key-comb :Cmd-Up (fn [editor] (p/forward-sexp editor)))
+     #_(key-comb :Cmd-Ctrl-Alt-P (fn [editor] (p/forward-sexp editor)))
+     (key-comb :Cmd-Ctrl-Alt-K (fn [editor] (fs/exec ["pkill" "--full" "boot"])))
+     (key-comb :Cmd-Ctrl-Alt-L (fn [editor] (fs/exec ["pgrep" "--full" "boot"])))
+     (key-comb :Shift-Cmd-D (fn [editor] (fs/exec ["ls" "-la"])))
+     (key-comb :Cmd-Ctrl-Alt-B (fn [editor] (fs/exec ["boot" "watch" "dev-build"]))))
    clj->js))
